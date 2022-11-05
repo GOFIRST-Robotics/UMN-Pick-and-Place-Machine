793d7379e89acd8bd5383a710065a4c70fa47d85
Anthony Brogni <anthonybrog@gmail.com>
2022-11-04 19:11:38 -0500

    start sequence

--- robotArm/dorna2_connection_test.py
+++ robotArm/dorna2_connection_test.py
@@ -1,7 +1,7 @@
 from dorna2 import Dorna  # this is the dorna2 python API
 
 robot = Dorna()  # Create a new instance of a "Dorna" object
-robot_ip_address = "10.164.161.152"  # Specify the ip address to connect to
+robot_ip_address = "169.254.63.192"  # Specify the ip address to connect to
 
 if not robot.connect(robot_ip_address):  # (Attempt to) Connect to the robot server at the specified IP address
     print("ERROR: Could not find the dorna2 robot at port:", robot_ip_address)
@@ -18,10 +18,16 @@ else:
 
     # Go to "working" position!
     print("Executing commands now")
-    robot.play(timeout=2, cmd="jmove", rel=0, j0=-90, vel=1200, cont=1)
-    robot.play(timeout=2, cmd="jmove", rel=0, j1=-160, vel=1200, cont=1)
-    robot.play(timeout=2, cmd="jmove", rel=0, j2=50, vel=1200, cont=1)
-    robot.play(timeout=2, cmd="jmove", rel=0, j3=-140, vel=1200, cont=1)
+    robot.play(timeout=2, cmd="jmove", rel=1, j0=-90, vel=120, accel=120, cont=1)
+    robot.play(timeout=2, cmd="jmove", rel=1, j1=-160, vel=120, accel=120, cont=1)
+    robot.play(timeout=2, cmd="jmove", rel=1, j2=50, vel=120, accel=120, cont=1)
+    robot.play(timeout=2, cmd="jmove", rel=1, j3=-150, vel=120, accel=120, cont=1)
+    # Got back to our home/origin
+    robot.play(timeout=2, cmd="jmove", rel=1, j3=150, vel=120, accel=120, cont=1)
+    robot.play(timeout=2, cmd="jmove", rel=1, j2=-50, vel=120, accel=120, cont=1)
+    robot.play(timeout=2, cmd="jmove", rel=1, j1=160, vel=120, accel=120, cont=1)
+    robot.play(timeout=2, cmd="jmove", rel=1, j0=90, vel=120, accel=120, cont=1)
+
 
 robot.close()  # Always close the socket when you are done :)
 print("Script is over now")
