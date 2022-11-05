167b2868180d49bfd55318d69d61b616abe170d0
Anthony Brogni <anthonybrog@gmail.com>
2022-11-04 19:59:40 -0500

    Added a successfully line move

--- robotArm/dorna2_connection_test.py
+++ robotArm/dorna2_connection_test.py
@@ -18,15 +18,28 @@ else:
 
     # Go to "working" position!
     print("Executing commands now")
-    robot.play(timeout=2, cmd="jmove", rel=1, j0=-90, vel=120, accel=120, cont=1)
-    robot.play(timeout=2, cmd="jmove", rel=1, j1=-160, vel=120, accel=120, cont=1)
-    robot.play(timeout=2, cmd="jmove", rel=1, j2=50, vel=120, accel=120, cont=1)
-    robot.play(timeout=2, cmd="jmove", rel=1, j3=-150, vel=120, accel=120, cont=1)
+    robot.play(timeout=2, cmd="jmove", rel=1, j0=-90, vel=120, accel=120)
+    print(robot.recv())
+    robot.play(timeout=2, cmd="jmove", rel=1, j1=-160, vel=120, accel=120)
+    print(robot.recv())
+    robot.play(timeout=2, cmd="jmove", rel=1, j2=50, vel=120, accel=120)
+    print(robot.recv())
+    robot.play(timeout=2, cmd="jmove", rel=1, j3=-150, vel=120, accel=120)
+    print(robot.recv())
+
+    robot.sleep(2)
+    robot.play(timeout=2, cmd="lmove", rel=0, x=0, y=375, z=140, vel=120, accel=120)
+    print(robot.recv())
+
     # Got back to our home/origin
-    robot.play(timeout=2, cmd="jmove", rel=1, j3=150, vel=120, accel=120, cont=1)
+    """robot.play(timeout=2, cmd="jmove", rel=1, j3=150, vel=120, accel=120, cont=1)
+    print(robot.recv())
     robot.play(timeout=2, cmd="jmove", rel=1, j2=-50, vel=120, accel=120, cont=1)
+    print(robot.recv())
     robot.play(timeout=2, cmd="jmove", rel=1, j1=160, vel=120, accel=120, cont=1)
+    print(robot.recv())
     robot.play(timeout=2, cmd="jmove", rel=1, j0=90, vel=120, accel=120, cont=1)
+    print(robot.recv())"""
 
 
 robot.close()  # Always close the socket when you are done :)
