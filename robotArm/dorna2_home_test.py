f0d44f935b344bada10578708aab3048a20afe57
Anthony Brogni <anthonybrog@gmail.com>
2022-11-11 17:24:04 -0600

    split our current test file into 2 separate tests
    -1 test for moving to the working position
    -another test for moving back to "home"

---
+++ robotArm/dorna2_home_test.py
@@ -0,0 +1,31 @@
+from dorna2 import Dorna  # this is the dorna2 python API
+
+robot = Dorna()  # Create a new instance of a "Dorna" object
+robot_ip_address = "169.254.63.192"  # Specify the ip address to connect to
+
+if not robot.connect(robot_ip_address):  # (Attempt to) Connect to the robot server at the specified IP address
+    print("ERROR: Could not find the dorna2 robot at port:", robot_ip_address)
+else:
+    print("Successfully connected to the dorna2 at port:", robot_ip_address)
+
+    # Disable the alarm mode
+    if robot.get_alarm():
+        robot.set_alarm(0)
+        print("Disabling the alarm")
+
+    robot.set_motor(1)  # Turn on the motors
+    print("Turning on the motors")
+
+    # Got back to our home/origin
+    print("Executing commands now")
+    robot.play(timeout=2, cmd="jmove", rel=1, j3=150, vel=120, accel=120)
+    print(robot.recv())
+    robot.play(timeout=2, cmd="jmove", rel=1, j2=-50, vel=120, accel=120)
+    print(robot.recv())
+    robot.play(timeout=2, cmd="jmove", rel=1, j1=160, vel=120, accel=120)
+    print(robot.recv())
+    robot.play(timeout=2, cmd="jmove", rel=1, j0=90, vel=120, accel=120)
+    print(robot.recv())
+
+robot.close()  # Always close the socket when you are done :)
+print("Script is over now")
