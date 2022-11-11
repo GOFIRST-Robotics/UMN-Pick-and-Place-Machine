cc6245248ca42d0b254db9fde0056c5f95b56926
Anthony Brogni <anthonybrog@gmail.com>
2022-11-11 17:45:55 -0600

    Created Test folder and made basic connection test

---
+++ robotArm/Tests/dorna2_connection_test.py
@@ -0,0 +1,12 @@
+from dorna2 import Dorna  # this is the dorna2 python API
+
+robot = Dorna()  # Create a new instance of a "Dorna" object
+robot_ip_address = "169.254.63.192"  # Specify the ip address to connect to
+
+if not robot.connect(robot_ip_address):  # (Attempt to) Connect to the robot server at the specified IP address
+    print("ERROR: Could not find the dorna2 robot at port:", robot_ip_address)
+else:
+    print("Successfully connected to the dorna2 at port:", robot_ip_address)
+
+robot.close()  # Always close the socket when you are done :)
+print("Script is over now")
